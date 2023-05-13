b5cead2574f393824fd6d094b5d59443960379b7
Evgeny Stepanischev <imbolk@gmail.com>
2023-05-13 16:18:26 +0300

    Replacing C-strings with C++-string

--- src/comm/so.cpp
+++ src/comm/so.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <fmt/format.h>
+
 #include <gopnik2/comm/comm.h>
 #include <gopnik2/main.h>
 #include <gopnik2/list.h>
@@ -10,15 +13,18 @@ int so() {
     int i;
 
     for (i = 1; i < STR_AMOUNT; i++) {
-        settextattr(GREEN);
-        PRINTF(
-                "%s %i",
+        std::cout
+            << GREEN
+            << fmt::format("{} {}",
                 cur_game->str_hero[i]->get_type(),
-                cur_game->str_hero[i]->get_level());
-        PRINTF(
-                " (%i/%i)\n",
+                cur_game->str_hero[i]->get_level()
+            )
+            << fmt::format(
+                " ({}/{})\n",
                 cur_game->str_hero[i]->get_health(),
-                cur_game->str_hero[i]->get_max_health());
+                cur_game->str_hero[i]->get_max_health()
+            )
+            << std::flush;
     }
 
     return 0;
