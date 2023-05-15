0e1e0caa40f12351db7febe7065bd98e66a5c5b0
Evgeny Stepanischev <imbolk@gmail.com>
2023-05-15 19:48:59 +0300

    Replacing C-strings with C++-string

--- src/comm/d.cpp
+++ src/comm/d.cpp
@@ -1,4 +1,7 @@
 #include <cstdlib>
+#include <iostream>
+
+#include <fmt/format.h>
 
 #include <gopnik2/comm/comm.h>
 #include <gopnik2/main.h>
@@ -13,16 +16,17 @@ int d() {
     hero *main_hero;
 
     // сообщения функции
-    const char *mess[] = {
+    const std::string mess[] = {
             "Ты пошёл с пацанами на дело...\n",
             "Вы вымолотоли хату и надыбали бабла\n",
             "Ты подсобил пацанам. Они этого не забудут\n",
-            "Твоя понтовость в притоне повысилась на %i\n",
+            "Твоя понтовость в притоне повысилась на {}\n",
             "-Шухер! Мусора!\n",
             "Вас кто-то запалил\n",
             "Вы успели наскоряк свалить.\n",
-            "Вы уже собрались делать ноги, но тут на вашем пути встал Мент %i уровня\n",
-            "-А ну стоять!\n"};
+            "Вы уже собрались делать ноги, но тут на вашем пути встал Мент {} уровня\n",
+            "-А ну стоять!\n",
+    };
 
     int
     // индекс генерируемого типа героя
@@ -35,23 +39,19 @@ int d() {
     if (cur_game->get_open_d()) {
         main_hero = cur_game->main_hero;
 
-        settextattr(YELLOW);
-        PRINTF("%s", mess[0]);
+        std::cout << YELLOW << mess[0];
 
         get_key();
 
         for (;;) {
             // вероятность, что повезёт
             if (CHANCE(1, 20)) {
-                settextattr(YELLOW);
-                PRINTF("%s", mess[1]);
-                PRINTF("%s", mess[2]);
+                std::cout << YELLOW << mess[1] << mess[2];
 
                 main_hero->add_att(20);
                 main_hero->add_money(GETRANDOM(19, 20 + 20 * main_hero->district));
 
-                settextattr(BLUE);
-                PRINTF(mess[3], 20);
+                std::cout << BLUE << fmt::format(mess[3], 20) << std::flush;
 
                 // переход к новой локации
                 cur_game->set_loc(4);
@@ -63,14 +63,11 @@ int d() {
 
             // вероятность, что не повезёт
             if (CHANCE(1, 20)) {
-                settextattr(YELLOW);
-                PRINTF("%s", mess[4]);
-
-                settextattr(RED);
-                PRINTF("%s", mess[5]);
-
-                settextattr(YELLOW);
-                PRINTF("%s", mess[6]);
+                std::cout
+                    << YELLOW << mess[4]
+                    << RED << mess[5]
+                    << YELLOW << mess[6]
+                    << std::flush;
 
                 // переход к новой локации
                 cur_game->set_loc(4);
@@ -82,20 +79,16 @@ int d() {
 
             // вероятность, что наедут
             if (CHANCE(1, 10)) {
-                settextattr(YELLOW);
-                PRINTF("%s", mess[4]);
-
-                settextattr(RED);
-                PRINTF("%s", mess[5]);
+                std::cout
+                    << YELLOW << mess[4]
+                    << RED << mess[5];
 
                 ht_index = cur_game->search_ht("Мент");
                 level = GETRANDOM(main_hero->get_level() + 1, main_hero->get_level() + 5);
 
-                settextattr(YELLOW);
-                PRINTF(mess[7], level);
-
-                settextattr(RED);
-                PRINTF("%s", mess[8]);
+                std::cout
+                << YELLOW << fmt::format(mess[7], level)
+                << RED << mess[8] << std::flush;
 
                 cur_game->gen_enemy_obj(ht_index, level);
 
